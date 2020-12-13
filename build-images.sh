d270d2e9427f5cbc808881af2ff8e18928a4fc9f
Hridya Krishna <hridyakrishnar127@gmail.com>
2020-12-13 01:48:59 -0800

    Fixed incorrect directory name

--- build-images.sh
+++ build-images.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 pushd Gateway
-docker build -t gateway-svc:0.1 .
+docker build -t gateway-svc:0.1 . || exit -1
 popd
 
 pushd UI
-docker build -t ui-svc:0.1 .
+docker build -t ui-svc:0.1 . || exit -1
 popd
 
-pushd Booking
-docker build -t booking-svc:0.1 .
+pushd Booking_service
+docker build -t booking-svc:0.1 . || exit -1
 popd
 
-pushd Restaurant
-docker build -t restaurant-svc:0.1 .
+pushd Restaurant_service
+docker build -t restaurant-svc:0.1 . || exit -1
 popd
 
-pushd User
-docker build -t user-svc:0.1 .
+pushd User_service
+docker build -t user-svc:0.1 . || exit -1
 popd
 
 pushd DB
-docker build -t db-svc:0.1 .
+docker build -t db-svc:0.1 . || exit -1
 popd
 
